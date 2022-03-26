153545fb24e0308784c6075e3fed41e491016775
cagdas <cagdas.oztekin@mobilearts.com>
2022-03-27 00:37:00 +0100

    Add very crude backend functions and an initial todo lists object.

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,37 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+let todos = {
+    '0000000001': {
+        id: '0000000001',
+        title: 'First List',
+        todos: [{ text: 'First todo of first list!', dueDate: '2022-03-27', dueTime: '17:00' }],
+        completedTodos: [],
+    },
+    '0000000002': {
+        id: '0000000002',
+        title: 'Second List',
+        todos: [{ text: 'First todo of second list!', dueDate: '2022-02-10', dueTime: '08:00' }],
+        completedTodos: []
+    }
+};
+
 app.use(cors())
 app.use(express.json())
 
 const PORT = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/get_todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/save_todo', (req, res) => {
+    const { body = {} } = req;
+    const { id = '', list = {} } = body;
+    if (id !== '') {
+        todos[id] = list;
+    }
+    res.status(200).send('Success');
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
